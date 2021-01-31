1dbc52713ecb27ab4af7e62df9bdbbca6a8d8262
David Calhoun <dpcalhoun@gmail.com>
2021-01-31 15:53:38 -0600

    Remove MDX config and scripts

--- next.config.js
+++ next.config.js
@@ -3,21 +3,17 @@ const withTM = require("next-transpile-modules")(
   ["bs-platform"].concat(bsconfig["bs-dependencies"])
 );
 
-let withMDX = require("@next/mdx")();
-
-module.exports = withMDX(
-  withTM({
-    pageExtensions: ["mdx", "js", "bs.js"],
-    webpack: (config) => {
-      return {
-        ...config,
-        // Mock `fs` module for client packages
-        // https://webpack.js.org/configuration/node/#node
-        node: {
-          fs: "empty",
-          module: "empty",
-        },
-      };
-    },
-  })
-);
+module.exports = withTM({
+  pageExtensions: ["js", "bs.js"],
+  webpack: (config) => {
+    return {
+      ...config,
+      // Mock `fs` module for client packages
+      // https://webpack.js.org/configuration/node/#node
+      node: {
+        fs: "empty",
+        module: "empty",
+      },
+    };
+  },
+});
